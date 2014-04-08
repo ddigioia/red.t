8cba369b55c54dde4bc9f9553a8796ea1ce59f27
stephenhmarsh <stephenforpresident@gmail.com>
2014-04-08 18:47:04 -0400

    added tests for password and email uniqueness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,15 @@ require 'spec_helper'
 describe User do
   
 it {should have_secure_password}
-it {should validate_uniqueness_of(:email)}
+
+ it do
+    patrick = User.create!({
+    name: 'Patrick',
+    email: 'pat@rick.com',
+    password: 'spongebob',
+    password_confirmation: 'spongebob',
+  })
+    should validate_uniqueness_of(:email)
+  end
 
 end
