58cc8e82072b86fc49c2204d8796c74150f7e90e
stephenhmarsh <stephenforpresident@gmail.com>
2014-04-08 21:20:38 -0400

    added some basic auth, added dependent destroy on links when user is deleted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+	before_action :require_authentication, only: [:edit, :update, :destroy]
+
 def index
 	@users = User.all
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 
 	has_secure_password
-	has_many :links
+	has_many :links, dependent: :destroy
 	
 	validates :email, uniqueness: true
 	validates :name, :email, presence: true
